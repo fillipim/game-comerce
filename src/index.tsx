0e99e8ae3cdf508273fea8f912a51327b83fb07e
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2023-02-16 10:22:02 -0300

    feat: add reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/Cart';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-  
+    <Provider store={store}>
+
+    </Provider>
   </React.StrictMode>
 );
 
