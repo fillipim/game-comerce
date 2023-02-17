916670b65c9cca7d6abb60040f6b76eeeb8d197a
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2023-02-17 14:20:54 -0300

    refactor: refactor the cart function

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,29 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
-import { toggleCart } from "store/Cart/cart.slice";
 import { ImCart } from "react-icons/im";
 
 import ProductCart from "components/ProductCart";
 import { selectCart } from "store/Cart/cart.slice";
 import * as S from "./ShoppingCart.style";
+import { useState } from "react";
 
 const ShoppingCart = () => {
+  const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
   const cartState = useSelector(selectCart);
-  const dispach = useDispatch();
 
-  return cartState.isOpen ? (
+  const handleCart = () =>
+    cartIsOpen ? setCartIsOpen(false) : setCartIsOpen(true);
+
+  return cartIsOpen ? (
     <S.Cart>
       <S.CartTitle tag="h3" size="size5">
         Carrinho de compras
       </S.CartTitle>
       <S.CartList>
-        {cartState.items.map((game) => (
+        {cartState.map((game) => (
           <ProductCart game={game} />
         ))}
       </S.CartList>
     </S.Cart>
   ) : (
-    <S.CartButton onClick={() => dispach(toggleCart())}>
-      <ImCart/>
+    <S.CartButton onClick={handleCart}>
+      <ImCart />
     </S.CartButton>
   );
 };
--- src/store/Cart/cart.slice.ts
+++ src/store/Cart/cart.slice.ts
@@ -3,49 +3,22 @@ import { RootState } from ".";
 import { IGameCart } from "interfaces";
 
 export interface ICartState {
-    cart: {
-        isOpen: boolean,
-        items: IGameCart[]
-    }
+    cart: IGameCart[]
 }
 
 const initialState = {
-    cart: {
-        isOpen: false,
-        items: [{
-            "id": 312,
-            "name": "Super Mario Odyssey",
-            "price": 197.88,
-            "score": 100,
-            "image": "super-mario-odyssey.png",
-            "amount": 1
-          },
-          {
-            "id": 201,
-            "name": "Call Of Duty Infinite Warfare",
-            "price": 49.99,
-            "score": 80,
-            "image": "call-of-duty-infinite-warfare.png",
-            "amount": 1
-          }]
-    }
+    cart: []
 }
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        toggleCart (state) {
-            if(state.cart.isOpen){
-                state.cart.isOpen = false
-                return;
-            }
-            state.cart.isOpen = true
-        }
+      
     }
 })
 
-export const { toggleCart } = cartSlice.actions;
+export const {  } = cartSlice.actions;
 
 export const selectCart = (state: RootState) => state.cart
 
